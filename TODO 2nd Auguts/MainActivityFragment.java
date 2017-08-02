9faa5b451613ec855e48843eb6f7ae8d34e03286
Mitali Pattani <sonarmitali19@gmail.com>
2017-08-02 10:15:33 -0700

    Update MainActivityFragment.java

--- TODO 2nd Auguts/MainActivityFragment.java
+++ TODO 2nd Auguts/MainActivityFragment.java
@@ -30,5 +30,58 @@
         String countryName = getCountryName(correctAnswer);
         ((Button) randomRow.getChildAt(column)).setText(countryName);
         
-        
+-----------------------------------------------------------------------------------------------------------------------
+//PLACE THIS CODE IN ONCREATEVIEW()
+//TODO 15) Add the listeneer for all buttons
+        // configure listeners for the guess Buttons
+        for (LinearLayout row : guessLinearLayouts) {
+            for (int column = 0; column < row.getChildCount(); column++) {
+                Button button = (Button) row.getChildAt(column);
+                button.setOnClickListener(guessButtonListener);
+            }
+        }
+---------------------------------------------------------------------------------------------------------------------------
+//CREATE A NEW METHOD FOR LISTENER
+//TODO 16) Add code in the OnClick() 
+         private View.OnClickListener guessButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Button guessButton = ((Button) v);
+            String guess = ((Button) v).getText().toString();
+            String answer = getCountryName(correctAnswer);
+
+            //if the guess is correct
+            // display correct answer in green text
+            if(guess.equals(answer))
+            {
+                count++;
+                answerTextView.setText(answer + "!");
+         //set the text in green Color
+                disableAllButtons();
+            }
+            else
+            {
+              //  flagImageView.startAnimation(shakeAnimation); // play shake
+
+                // display "Incorrect!" in red
+                answerTextView.setText(R.string.incorrect_answer);
+              //set the text in red Color
+                guessButton.setEnabled(false); // disable incorrect answer
+            }
+
+        }
+        };
+
+    // utility method that disables all answer Buttons
+        public void disableAllButtons()
+        {
+            for(int row=0;row<guessRows;row++)
+            {
+                LinearLayout guessRow = guessLinearLayouts[row];
+                for(int i=0;i<guessRow.getChildCount();i++)
+                {
+                    guessRow.getChildAt(i).setEnabled(false);
+                }
+            }
+        }
       
